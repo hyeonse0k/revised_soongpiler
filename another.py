ccafb2bca4b3541b7dc00ca8a381236db131da3e
CMhs <ktf3116@naver.com>
2020-05-23 20:12:27 +0900

    ADD python code about sorting image files

---
+++ another.py
@@ -0,0 +1,3 @@
+#!C:\Users\gkrtj\AppData\Local\Programs\Python\Python37\python.exe
+print("content-type:text/html; charset=UTF-8\n")
+print(1+1)
--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-#!C:\Users\\rhfmr\AppData\Local\Microsoft\WindowsApps\python.exe
+#!C:\Users\gkrtj\AppData\Local\Programs\Python\Python37\python.exe
+import os
+import sys
+import shutil
 print("content-type:text/html; charset=UTF-8\n")
+path = "C:/Bitnami/wampstack-7.3.17-0/apache2/htdocs/Controller/photos/"
+class_list = os.listdir(path)
 
-print(1+1)
+for CLASS in class_list:
+    file_name = os.listdir(path + CLASS + "/")
+    os.mkdir(path + CLASS + "/" + "train")
+    os.mkdir(path + CLASS + "/" + "test")
+    cnt = 1
+    for file in file_name:
+        if cnt % 4 == 0:
+            shutil.move(path + CLASS + "/" + file, path + CLASS + "/test/" + file)
+        else:
+            shutil.move(path + CLASS + "/" + file, path + CLASS + "/train/" + file)
+        cnt += 1
